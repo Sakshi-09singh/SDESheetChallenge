b65fa66bd85a2c3f962ba5f47f4679644a7a7973
Sakshi09i <sakshisingh9906@gmail.com>
2026-06-01 22:36:41 +0530

    Day 1 of SDESheetChallenge

---
+++ NextPermutation.java
@@ -0,0 +1,44 @@
+
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+
+        int i = n - 1;
+
+        for (; i > 0; i--) {
+            if (nums[i] > nums[i - 1]) {
+                break;
+            }
+        }
+
+        if (i != 0) {
+            int index = i;
+
+            for (int j = n - 1; j >= i; j--) {
+                if (nums[j] > nums[i - 1]) {
+                    index = j;
+                    break;
+                }
+            }
+
+            // Swap
+            int temp = nums[i - 1];
+            nums[i - 1] = nums[index];
+            nums[index] = temp;
+        }
+
+        // Reverse the remaining array
+        reverse(nums, i, n - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+}
